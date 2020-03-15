9ebb7082a24f10b0163944432be0f73e38fb72b1
Maite <maitecondepalacio@gmail.com>
2020-03-15 18:19:30 +0100

    css

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-axios.get('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&page=1')
+axios.get('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=en-EN&page=1')
     .then(res => {
         const peliculas = res.data.results;
         peliculasContainer.innerHTML = '';
         const baseImgUrl = 'https://image.tmdb.org/t/p/w185';
         peliculas.forEach(pelicula => {
             const imagen = pelicula.poster_path ? `
-        <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : '<img src="./img/image-not-available.jpg"'
+        <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : '<img src="./img/image-not-available.jpg" class ="noDis"'
             peliculasContainer.innerHTML += `
-        <div class="pelicula">
-            <h3 class="title">${pelicula.title}
-        </h3>
+
+        <div onclick = "renderPeli(${pelicula.id})"  class="pelicula">
         ${imagen}
+        <div class = "info">
+            <h3 class="title">${pelicula.title} 
+        </h3>
+       <p> ${pelicula.release_date} </p>
+      </div>
     </div>`
         })
     })
@@ -20,7 +24,7 @@ const peliculasContainer = document.querySelector('main.peliculas');
 document.querySelector('.buscarInput')
     .addEventListener('keyup', event => {
         if (event.key === 'Enter') {
-            fetch('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query=' + event.target.value)
+            fetch('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=en-EN&query=' + event.target.value)
                 .then(res => res.json())
                 .then(res => {
                     const peliculas = res.results;
@@ -32,17 +36,24 @@ document.querySelector('.buscarInput')
                     if (long > 0) {
                         peliculas.forEach(pelicula => {
                             const imagen = pelicula.poster_path ? `
-                    <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : '<img src="./img/notyetavailablekyle.jpg"'
+                    <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : '<img src="./img/image-not-available.jpg" class ="noDis"'
 
                             peliculasContainer.innerHTML += `
-                    <div class="pelicula">
-                <h3 class="title">${pelicula.title}
-                </h3>
-                ${imagen}
-            </div>`
+
+                            <div onclick = "renderPeli(${pelicula.id})"  class="pelicula">
+                            ${imagen}
+                            <div class = "info">
+                                <h3 class="title">${pelicula.title} 
+                            </h3>
+                           <p> ${pelicula.release_date} </p>
+                          </div>
+                        </div>`
                         })
                     } else {
-                        peliculasContainer.innerHTML += `<div> <h2>No hay coincidencias</h2> </div>`
+                        peliculasContainer.innerHTML += `<div class ="noCoin"> 
+                        <img src="./img/pngguru.com.png" alt="">
+                        <h2>No se han encontrado películas con esta búsqueda</h2>
+                        </div>`
                     }
 
                 })
@@ -51,7 +62,7 @@ document.querySelector('.buscarInput')
 
 
 
-axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES')
+axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=cea68b520beecac6718820e4ac576c3a&language=en-EN')
     .then(res => {
         const generos = res.data.genres;
         generos.forEach(genero => {
@@ -62,33 +73,7 @@ axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=cea68b520beecac
     })
 
     
-//     pelisGen = (event, genreId) => {
-//         axios.get ('https://api.themoviedb.org/3/discover/movie?api_key=cea68b520beecac6718820e4ac576c3a&with_genres=${genreId}')
-//         .then(res => {
-//             const peliculas = res.data.results;
-//             const long = peliculas.length
-//      document.querySelector('main.peliculas').innerHTML = '';
-//             const baseImgUrl = 'https://image.tmdb.org/t/p/w185';
-//             console.log(peliculas)
-//             console.log(long)
-//             if (long > 0) {
-//                 peliculas.forEach(pelicula => {
-//                     const imagen = pelicula.poster_path ? `
-//             <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : '<img src="./img/notyetavailablekyle.jpg"'
 
-//             document.querySelector('main.peliculas').innerHTML += `
-//             <div class="pelicula">
-//         <h3 class="title">${pelicula.title}
-//         </h3>
-//         ${imagen}
-//     </div>`
-//                 })
-//             } else {
-//                 peliculasContainer.innerHTML += ''
-//             }
-
-//         })
-// }
 const pelisGen = (event, genreId) => {
     axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=cea68b520beecac6718820e4ac576c3a&with_genres=${genreId}`)
         .then(res => {
@@ -98,12 +83,16 @@ const pelisGen = (event, genreId) => {
                 document.querySelector('main.peliculas').innerHTML = '';
                 peliculas.forEach(pelicula => {
                     const imagen = pelicula.poster_path ? `
-                               <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : '<img src="./img/notyetavailablekyle.jpg"'
+                               <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : '<img src="./img/image-not-available.jpg" class ="noDis"'
                     document.querySelector('main.peliculas').innerHTML += `
-                <div class='pelicula'>
-                <h3 class="title">${pelicula.title}
-                 </h3>
-                ${imagen}
+
+                    <div onclick = "renderPeli(${pelicula.id})"  class="pelicula">
+                    ${imagen}
+                    <div class = "info">
+                        <h3 class="title">${pelicula.title} 
+                    </h3>
+                   <p> ${pelicula.release_date} </p>
+                  </div>
                 </div>`;
                 })
             } else {
@@ -112,3 +101,52 @@ const pelisGen = (event, genreId) => {
         })
         .catch(error => console.error(error))
     }
+
+    const renderPeli = id => {
+       
+        axios.get(` https://api.themoviedb.org/3/movie/${id}?api_key=cea68b520beecac6718820e4ac576c3a&append_to_response=credits`)
+    .then(res => {
+        const pelicula = res.data;
+        const baseImgUrl = 'https://image.tmdb.org/t/p/w185';
+            const imagen = pelicula.poster_path ? `
+        <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : '<img src="./img/image-not-available.jpg" class ="noDis"'
+       
+            peliculasContainer.innerHTML =   `
+        <div  class="peli">
+        ${imagen}
+        <div class ="infos">
+            <h3 class="title">${pelicula.title}
+        </h3>
+       <p>${pelicula.overview} </p>
+     
+      
+       </div>
+    </div>` 
+  
+ })
+     }
+ 
+    const goIinicio = () => {
+        axios.get('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=en-EN&page=1')
+    .then(res => {
+        const peliculas = res.data.results;
+        peliculasContainer.innerHTML = '';
+        const baseImgUrl = 'https://image.tmdb.org/t/p/w185';
+        peliculas.forEach(pelicula => {
+            const imagen = pelicula.poster_path ? `
+        <img src="${baseImgUrl}${pelicula.poster_path}" alt="">` : '<img src="./img/image-not-available.jpg" class ="noDis"'
+            peliculasContainer.innerHTML += `
+
+        <div onclick = "renderPeli(${pelicula.id})"  class="pelicula">
+        ${imagen}
+        <div class = "info">
+            <h3 class="title">${pelicula.title} 
+        </h3>
+       <p> ${pelicula.release_date} </p>
+      </div>
+    </div>`
+        })
+    })
+    }
+
+   
